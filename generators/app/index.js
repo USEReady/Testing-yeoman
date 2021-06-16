c65851263da6eed292f171ce5f583e3df07df1b6
kirank-useready <78422039+kirank-useready@users.noreply.github.com>
2021-06-16 19:45:31 +0530

    Add files via upload

---
+++ generators/app/index.js
@@ -0,0 +1,49 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+const mkdirp = require('mkdirp');
+
+module.exports = class extends Generator {
+  prompting() {
+   
+    const prompts = [
+      {
+        name: "name",
+        message: "Your project name",
+        default: "generated-app"
+
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+  default() {
+    mkdirp(this.props.name);
+    this.destinationRoot(this.destinationPath(this.props.name));
+  }
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
+    this.fs.copy(this.templatePath('*'), this.destinationPath());
+  this.fs.copy(this.templatePath('.*'), this.destinationPath());
+  this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
+  this.fs.copy(this.templatePath('public'), this.destinationPath('public'));
+this.fs.copy(this.templatePath('build'), this.destinationPath('build'));
+this.fs.copy(this.templatePath('SSL'), this.destinationPath('SSL'));
+  this.fs.copyTpl(
+    this.templatePath('package.json'),
+    this.destinationPath('package.json'),
+    this.props // template variables
+  );
+
+  }
+
+  install() {
+    this.installDependencies({ bower: false });
+  }
+};
