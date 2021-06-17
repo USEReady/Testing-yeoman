1c9a3be38faf56c0608c5f0ca28e9891089c6d20
Kiran Bajirao Kumbhar <kirank@useready.local>
2021-06-17 15:32:47 +0530

    Change js file extension

--- generators/app/index.js
+++ generators/app/index.js
@@ -39,7 +39,7 @@ module.exports = class extends Generator {
 //this.fs.copy(this.templatePath('build'), this.destinationPath('build'));
 this.fs.copy(this.templatePath('SSL'), this.destinationPath('SSL'));
   this.fs.copyTpl(this.templatePath('package.json'),this.destinationPath('package.json'),this.props);
-  this.fs.copyTpl(this.templatePath('server.json'),this.destinationPath('server.json'),this.props);
+  this.fs.copyTpl(this.templatePath('server.js'),this.destinationPath('server.js'),this.props);
   }
 
   install() {
